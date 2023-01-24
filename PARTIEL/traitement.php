f486fece77531907bddc6b7b7d7cd6497eb42337
Adrien Tandabany <tandabany.adrien@hotmail.fr>
2023-01-24 14:50:11 +0100

    Modification ficher traitement.php

--- PARTIEL/traitement.php
+++ PARTIEL/traitement.php
@@ -2,12 +2,12 @@
 
 require "ressources.php";
 
-$developpeur = array(new Developpeur(550,"Le Flock", "Louis-Marie", "lmleflock@test.net",
-"0606060606",2500, "Java new Developpeur(650,"Poirie", "Ethan", "ach@test.net", "0707070707",2750, "C++")) ;
-) ;
- 
-$manager = array() ;
+$developpeur = array(new Developpeur(550,"Le Flock", "lmleflock@test.net", "0606060606",2500, "Java"),
+new Developpeur(650,"Chellal", "ach@test.net", "0707070707",2750, "C++"),
+new Developpeur(151,"Katsiev", "amir@test.net", "0606060606",2750, "Kotlin")) ;
+"Louis-Marie", "Amine", "Amir",
 
+$manager = array(new Manager(500,"RIPAULT", "Alexis", "alexis@manager.net", "0606060606",4000, "DSI")) ;
 
 foreach ($developpeur as $value) {
 	echo $value->afficher();
